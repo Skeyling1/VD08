6f05b6f25257b3b5083dc8d7f89fe495f5678b1a
Skeyling1 <skeyling1@gmail.com>
2024-11-28 16:44:01 +0300

    vvv

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather = None
+    news = None
     if request.method == 'POST':
         city = request.form['city']
         weather = get_weather(city)
-    return render_template("index.html", weather=weather)
+        news = get_news()
+    return render_template("index.html", weather=weather, news=news)
 
 
 def get_weather(city):
@@ -17,6 +19,12 @@ def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={api_key}&units=metric"
     response = requests.get(url)
     return response.json()
+
+def get_news():
+    api_key = "2c905e26210848e2a0322f1ee94123de"
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+    response = requests.get(url)
+    return response.json().get('articles', [])
 
 if __name__ == '__main__':
     app.run(debug=True)
